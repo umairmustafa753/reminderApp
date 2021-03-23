dc240fb6b6d5822b9b98d0566f80588c799773c3
Umair Mustafa <umairmustafa753@gmail.com>
2021-03-23 15:32:22 +0500

    fix typo

--- react-native-expo/store/Actions/ActionTypes.ts
+++ react-native-expo/store/Actions/ActionTypes.ts
@@ -2,7 +2,9 @@ const ActionTypes = {
   USER: "user",
   USER_REQUST: "userRequst",
   GET_USERS: "getUsers",
-  GET_USERS_REQUST: "getUsersRequst"
+  GET_USERS_REQUST: "getUsersRequst",
+  PUSH_NOTIFICATIN: "pushNotification",
+  REQUST_PUSH_NOTIFICATIN: "requstPushNotification"
 };
 
 export default ActionTypes;
